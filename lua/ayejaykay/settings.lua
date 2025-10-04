8accaeb67a7b18faed46b3f3ea9bb471393f7fb8
Anthony Kahley <anthony.kahley@hms-networks.com>
2025-10-04 11:55:14 -0400

    cosmetics

--- lua/ayejaykay/settings.lua
+++ lua/ayejaykay/settings.lua
@@ -1,9 +1,11 @@
 print("settings.lua")
 
 vim.cmd [[hi Normal guibg=NONE ctermbg=NONE]]
+vim.cmd [[hi CursorLine guibg='NvimDarkCyan' ctermbg=NONE]]
 
 vim.cmd [[set number relativenumber]]
 
+vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = 'gray', bold = true })
 vim.api.nvim_set_hl(0, 'LineNr', { fg = 'white', bold = true })
-
+vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = 'gray', bold = true })
 
